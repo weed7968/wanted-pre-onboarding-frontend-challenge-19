66b3e3051dab0fc5cef1bf9bd0571c25155bf425
weed7968 <tlstjdcjf980809@gmail.com>
2024-03-13 22:47:38 +0900

    CSS 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import List from "./components/List";
+import styled from "styled-components";
+
+const Body = styled.div`
+  background-color: #1e1e1e;
+  color: #fff;
+  text-align: center;
+  padding-top: 50px;
+`;
+
 function App() {
   return (
-    <div>
+    <Body>
       <List />
-    </div>
+    </Body>
   );
 }
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,63 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addItem, removeItem } from "../store/listSlice";
+import styled from "styled-components";
+
+const Body = styled.div`
+  width: 400px;
+  margin: 0 auto;
+  border: 1px solid #fff;
+  border-radius: 10px;
+
+  ul {
+    margin: 10px;
+    padding-left: 10px;
+  }
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #fff;
+  border-radius: 10px;
+  padding: 10px 15px;
+  font-size: 16px;
+  color: #fff;
+  background-color: #1e1e1e;
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1);
+  height: 20px;
+  opacity: 1;
+
+  &:focus {
+    outline: none;
+    border-color: #4d90fe;
+  }
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  background-color: #1e1e1e;
+  color: #fff;
+  border: 1px solid #fff;
+  border-radius: 10px;
+  margin-left: 20px;
+  cursor: pointer;
+
+  &:hover {
+    border-color: #4d90fe;
+  }
+`;
 
 export default function List() {
   const [inputValue, setInputValue] = useState("");
@@ -14,23 +71,22 @@ export default function List() {
   };
 
   return (
-    <div>
-      <div>
-        <input
+    <Body>
+      <Header>
+        <Input
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button onClick={handleAdd}>추가</button>
-      </div>
+        <Button onClick={handleAdd}>추가</Button>
+      </Header>
       <ul>
         {list.map((item) => (
           <li key={item.id}>
             <span>{item.text}</span>
-
-            <button onClick={() => dispatch(removeItem(item.id))}>삭제</button>
+            <Button onClick={() => dispatch(removeItem(item.id))}>삭제</Button>
           </li>
         ))}
       </ul>
-    </div>
+    </Body>
   );
 }
